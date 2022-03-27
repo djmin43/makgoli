63edf8f7ff3aae34c460d9a16e7cb256a0ba36fc
jay <dj.min43@gmail.com>
2022-03-27 16:23:10 +0900

    first php commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+  define('NAME', 'jaymin')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>my first php</title>
+</head>
+<body>
+ <div>
+   <?php echo NAME?>
+ </div> 
+</body>
+</html>
