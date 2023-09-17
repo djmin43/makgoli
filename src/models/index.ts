e253fbfafec5e13b26b93649d9279ff8d70a8a0f
jay <dj.min43@gmail.com>
2023-09-17 15:50:15 +0900

    public routing

---
+++ src/models/index.ts
@@ -0,0 +1,10 @@
+export type MakgoliPlace = {
+  id: number;
+  owner: string;
+  selected: string;
+  founded: string;
+  address: string;
+  name: string;
+  category: string;
+  description: string;
+};
